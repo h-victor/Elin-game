475edb6a4cedbee2d3de4136501bda4b3f11da8f
h-victor <victor.heng@edu.esiee.fr>
2015-05-13 10:13:13 +0200

    Adding graphics + overlap2d
    Adding first draw of Elin
    using overlap2d and its library

--- desktop/src/com/mygdx/elin/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/elin/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.mygdx.elin.elin;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new elin(), config);
 	}
 }
