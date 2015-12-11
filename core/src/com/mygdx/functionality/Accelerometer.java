2020f03cafcb72deabd9805beb555727e6cc4657
jmika-coding <mika78500@hotmail.fr>
2015-12-11 17:02:47 +0100

    little clean up of Accelerometer.java

--- core/src/com/mygdx/functionality/Accelerometer.java
+++ core/src/com/mygdx/functionality/Accelerometer.java
@@ -15,7 +15,6 @@ public class Accelerometer {
     public boolean state1LandscapeNormal, state2FlatNormal, state3LandscapeInverse, state4FlatInverse, state5LandscapeNormal;
     public boolean state2FlatInverse, state4FlatNormal, state5LandscapeNormalInverse; // inverse return smartphone
 
-
     /* boolean to move the player in the good way in Elin Script */
     /* don't work with the actual boolean because it implies conflicts in rotation*/
     public boolean smartphoneLandscapeNormal2, smartphonePortraitNormal2;
@@ -66,233 +65,197 @@ public class Accelerometer {
         smartphonePortraitInverse = false;
 
         /* Normal mode */
-        if(accelerometerX == 0 && accelerometerY == 0 && (accelerometerZ > 6)){ //== 10 || accelerometerZ == 9 || accelerometerZ == 8)){
+        if(accelerometerX == 0 && accelerometerY == 0 && accelerometerZ > 6){
             smartphoneFlatNormal = true;
         }
-        else if((accelerometerX > 6)/*== 10 || accelerometerX == 9 || accelerometerX == 8)*/ && accelerometerY >= -1 && accelerometerY <= 1 && accelerometerZ < 8/*== 0*/) {
+        else if(accelerometerX > 6 && (accelerometerY >= -1 && accelerometerY <= 1) && accelerometerZ < 8/*== 0*/) {
             smartphoneLandscapeNormal = true;
-
-            //            wasSmartphoneLandscapeNormal = true;
-            //            wasSmartphoneLandscapeInverse = false;
 
             smartphoneLandscapeNormal2 = true; smartphonePortraitNormal2 = false;
             smartphoneLandscapeInverse2 = false; smartphonePortraitInverse2= false;
         }
-        else if(accelerometerX >= -1 && accelerometerX <= 1 && (accelerometerY > 6)/*== 10 || accelerometerY == 9 || accelerometerY == 8)*/ && accelerometerZ < 8/*== 0*/){
+        else if((accelerometerX >= -1 && accelerometerX <= 1) && accelerometerY > 6 && accelerometerZ < 8/*== 0*/){
             smartphonePortraitNormal = true;			
-
-            //            wasSmartphonePortraitInverse = true;
-            //            wasSmartphonePortraitNormal= false;
 
             smartphonePortraitNormal2 = true; smartphoneLandscapeNormal2 = false;
             smartphonePortraitInverse2= false; smartphoneLandscapeInverse2 = false;            
         }
 
         /* Inverse mode */
-        else if(accelerometerX == 0 && accelerometerY == 0 && (accelerometerZ < -6)/*== -10 || accelerometerZ == -9 || accelerometerZ == -8)*/){
+        else if(accelerometerX == 0 && accelerometerY == 0 && accelerometerZ < -6){
             smartphoneFlatInverse = true;
         }
-        else if((accelerometerX < -6)/*== -10 || accelerometerX == -9 || accelerometerX == -8)*/ && accelerometerY >= -1 && accelerometerY <= 1 && accelerometerZ < 8/*== 0*/){
+        else if(accelerometerX < -6 && (accelerometerY >= -1 && accelerometerY <= 1) && accelerometerZ < 8/*== 0*/){
             smartphoneLandscapeInverse = true;
-
-            //            wasSmartphoneLandscapeNormal = false;
-            //            wasSmartphoneLandscapeInverse = true;
 
             smartphoneLandscapeInverse2 = true; smartphonePortraitInverse2 = false;
             smartphoneLandscapeNormal2 = false; smartphonePortraitNormal2 = false;
         }
-        else if(accelerometerX >= -1 && accelerometerX <= 1 && (accelerometerY < -6)/*== -10 || accelerometerY == -9 || accelerometerY == -8)*/ && accelerometerZ < 8/*== 0*/){
+        else if((accelerometerX >= -1 && accelerometerX <= 1) && accelerometerY < -6 && accelerometerZ < 8/*== 0*/){
             smartphonePortraitInverse = true;			
-
-            //            wasSmartphonePortraitInverse = false;
-            //            wasSmartphonePortraitNormal= true;
 
             smartphonePortraitInverse2 = true; smartphoneLandscapeInverse2 = false;
             smartphonePortraitNormal2 = false; smartphoneLandscapeNormal2 = false;
         }
+    }
+
+    /* rotate camera to landscape to portrait */
+    public void rotateCamera(){
+        /* Activate the rotation of the camera Normal Mode*/
+        if(smartphoneLandscapeNormal /*&& isRotated*/){
+            if(wasSmartphonePortraitNormal){
+                this.camera.rotate(camera.direction, -90f);//90f); 
+                wasSmartphonePortraitNormal = false;
+            }
+            else if(wasSmartphonePortraitInverse){
+                this.camera.rotate(camera.direction, 90f);//-90f);
+                wasSmartphonePortraitInverse = false;
+            }
+            else if(wasSmartphoneLandscapeInverse){
+                this.camera.rotate(camera.direction, 180f);
+                wasSmartphoneLandscapeInverse = false;
+            }
+            wasSmartphoneLandscapeNormal = true;
+            this.camera.update();
+            // isRotated = false;
+        }
+        else if(smartphonePortraitNormal /*&& !isRotated*/){
+            if(wasSmartphoneLandscapeNormal){
+                camera.rotate(camera.direction, 90f); 
+                wasSmartphoneLandscapeNormal = false;
+            }
+            else if(wasSmartphoneLandscapeInverse){
+                camera.rotate(camera.direction, -90f);
+                wasSmartphoneLandscapeInverse = false;
+            }
+            else if(wasSmartphonePortraitInverse){
+                camera.rotate(camera.direction, 180f);
+                wasSmartphonePortraitInverse = false;
+            }
+            wasSmartphonePortraitNormal = true;
+            camera.update();
+            // isRotated = true;
         }
 
-        /* rotate camera to landscape to portrait */
-        public void rotateCamera(){
-            /* Activate the rotation of the camera Normal Mode*/
-            if(smartphoneLandscapeNormal /*&& isRotated*/){
-                if(wasSmartphonePortraitNormal){
-                    this.camera.rotate(camera.direction, -90f);//90f); 
-
-                    wasSmartphonePortraitNormal = false;
-                }
-                else if(wasSmartphonePortraitInverse){
-                    this.camera.rotate(camera.direction, 90f);//-90f);
-
-                    wasSmartphonePortraitInverse = false;
-                }
-                else if(wasSmartphoneLandscapeInverse){
-                    this.camera.rotate(camera.direction, 180f);
-
-                    wasSmartphoneLandscapeInverse = false;
-                }
-
-                wasSmartphoneLandscapeNormal = true;
-
-                this.camera.update();
-                // isRotated = false;
+        /* Activate the rotation of the camera Inverse Mode*/
+        else if(smartphoneLandscapeInverse /*&& isRotated*/){
+            if(wasSmartphonePortraitInverse){
+                camera.rotate(camera.direction, -90f);//90f);
+                wasSmartphonePortraitInverse = false;
             }
-            else if(smartphonePortraitNormal /*&& !isRotated*/){
-                if(wasSmartphoneLandscapeNormal){
-                    camera.rotate(camera.direction, 90f); 
-
-                    wasSmartphoneLandscapeNormal = false;
-                }
-                else if(wasSmartphoneLandscapeInverse){
-                    camera.rotate(camera.direction, -90f);
-
-                    wasSmartphoneLandscapeInverse = false;
-                }
-                else if(wasSmartphonePortraitInverse){
-                    camera.rotate(camera.direction, 180f);
-
-                    wasSmartphonePortraitInverse = false;
-                }
-
-                wasSmartphonePortraitNormal = true;
-
-                camera.update();
-                // isRotated = true;
+            else if(wasSmartphonePortraitNormal){
+                camera.rotate(camera.direction, 90f);//-90f);
+                wasSmartphonePortraitNormal = false;
             }
-
-            /* Activate the rotation of the camera Inverse Mode*/
-            else if(smartphoneLandscapeInverse /*&& isRotated*/){
-                if(wasSmartphonePortraitInverse){
-                    camera.rotate(camera.direction, -90f);//90f);
-
-                    wasSmartphonePortraitInverse = false;
-                }
-                else if(wasSmartphonePortraitNormal){
-                    camera.rotate(camera.direction, 90f);//-90f);
-
-                    wasSmartphonePortraitNormal = false;
-                }
-                else if(wasSmartphoneLandscapeNormal){
-                    camera.rotate(camera.direction, 180f);
-
-                    wasSmartphoneLandscapeNormal = false;
-                }
-
-                wasSmartphoneLandscapeInverse = true;
-
-                camera.update();
-                // isRotated = false;
+            else if(wasSmartphoneLandscapeNormal){
+                camera.rotate(camera.direction, 180f);
+                wasSmartphoneLandscapeNormal = false;
             }
-            else if(smartphonePortraitInverse /*&& !isRotated*/){
-                if(wasSmartphoneLandscapeNormal){
-                    camera.rotate(camera.direction, -90f); 
-
-                    wasSmartphoneLandscapeNormal = false;
-                }
-                else if(wasSmartphoneLandscapeInverse){
-                    camera.rotate(camera.direction, 90f);
-
-                    wasSmartphoneLandscapeInverse = false;
-                }
-                else if(wasSmartphonePortraitNormal){
-                    camera.rotate(camera.direction, 180f);
-
-                    wasSmartphonePortraitNormal = false;
-                }
-
-                wasSmartphonePortraitInverse = true;
-
-                camera.update();
-                // isRotated = true;
-            }
+            wasSmartphoneLandscapeInverse = true;
+            camera.update();
+            // isRotated = false;
         }
-
-        /*******Function Return the smartphone*******/
-        public boolean returnSmartphoneNormal(){
-            if(smartphoneLandscapeNormal && !state4FlatInverse){
-                state1LandscapeNormal = true;
-
-                state5LandscapeNormal = false;
+        else if(smartphonePortraitInverse /*&& !isRotated*/){
+            if(wasSmartphoneLandscapeNormal){
+                camera.rotate(camera.direction, -90f); 
+                wasSmartphoneLandscapeNormal = false;
             }
-
-            else if(smartphoneFlatNormal && state1LandscapeNormal)			
-                state2FlatNormal = true;
-
-            else if(smartphoneLandscapeInverse && state2FlatNormal)
-                state3LandscapeInverse = true;
-
-            else if(smartphoneFlatInverse && state3LandscapeInverse)
-                state4FlatInverse = true;
-
-            else if(smartphoneLandscapeNormal && state4FlatInverse){
-                state5LandscapeNormal = true;
-
-                state2FlatNormal = false;
-                state3LandscapeInverse = false;
-                state4FlatInverse = false;
+            else if(wasSmartphoneLandscapeInverse){
+                camera.rotate(camera.direction, 90f);
+                wasSmartphoneLandscapeInverse = false;
             }
-
-            return state5LandscapeNormal;
-        }
-
-
-        public boolean returnSmartphoneinverse(){
-            if(smartphoneLandscapeNormal && !state4FlatNormal){
-                state1LandscapeNormal = true;
-
-                state5LandscapeNormalInverse = false;
+            else if(wasSmartphonePortraitNormal){
+                camera.rotate(camera.direction, 180f);
+                wasSmartphonePortraitNormal = false;
             }
-
-            else if(smartphoneFlatInverse && state1LandscapeNormal)			
-                state2FlatInverse = true;
-
-            else if(smartphoneLandscapeInverse && state2FlatInverse)
-                state3LandscapeInverse = true;
-
-            else if(smartphoneFlatNormal && state3LandscapeInverse)
-                state4FlatNormal = true;
-
-            else if(smartphoneLandscapeNormal && state4FlatNormal){
-                state5LandscapeNormalInverse = true;
-
-                state2FlatInverse = false;
-                state3LandscapeInverse = false;
-                state4FlatNormal = false;
-            }
-
-            return state5LandscapeNormalInverse;
-        }
-
-
-        /* raise a little the smartphone */
-        boolean raise = false;
-        public boolean raiseALittleSmartphone(){
-            if(wasSmartphoneLandscapeNormal && accelerometerX == 5){
-                raise = true;
-                //isRaise = true;
-            }
-            else{
-                if(raise && smartphoneLandscapeNormal){isRaise = true; raise = false;}
-                else{isRaise = false;}
-            }
-
-            return isRaise;
-        }
-
-        public boolean isPortraitNormal() {
-            // System.out.println("isPortraitNormal = " + smartphonePortraitNormal2);
-            return smartphonePortraitNormal2;
-        }
-        public boolean isLandscapeNormal() {
-            // System.out.println("isLandscapeNormal= " + smartphoneLandscapeNormal2);
-            return smartphoneLandscapeNormal2;
-        }
-        public boolean isPortraitInverse() {
-            // System.out.println("isPortraitNormal = " + smartphonePortraitNormal2);
-            return smartphonePortraitInverse2;
-        }
-        public boolean isLandscapeInvese() {
-            // System.out.println("isLandscapeNormal= " + smartphoneLandscapeNormal2);
-            return smartphoneLandscapeInverse2;
+            wasSmartphonePortraitInverse = true;
+            camera.update();
+            // isRotated = true;
         }
     }
+
+    /*******Function Return the smartphone*******/
+    public boolean returnSmartphoneNormal(){
+        if(smartphoneLandscapeNormal && !state4FlatInverse){
+            state1LandscapeNormal = true;
+
+            state5LandscapeNormal = false;
+        }
+
+        else if(smartphoneFlatNormal && state1LandscapeNormal)			
+            state2FlatNormal = true;
+
+        else if(smartphoneLandscapeInverse && state2FlatNormal)
+            state3LandscapeInverse = true;
+
+        else if(smartphoneFlatInverse && state3LandscapeInverse)
+            state4FlatInverse = true;
+
+        else if(smartphoneLandscapeNormal && state4FlatInverse){
+            state5LandscapeNormal = true;
+
+            state2FlatNormal = false;
+            state3LandscapeInverse = false;
+            state4FlatInverse = false;
+        }
+
+        return state5LandscapeNormal;
+    }
+
+
+    public boolean returnSmartphoneinverse(){
+        if(smartphoneLandscapeNormal && !state4FlatNormal){
+            state1LandscapeNormal = true;
+
+            state5LandscapeNormalInverse = false;
+        }
+
+        else if(smartphoneFlatInverse && state1LandscapeNormal)			
+            state2FlatInverse = true;
+
+        else if(smartphoneLandscapeInverse && state2FlatInverse)
+            state3LandscapeInverse = true;
+
+        else if(smartphoneFlatNormal && state3LandscapeInverse)
+            state4FlatNormal = true;
+
+        else if(smartphoneLandscapeNormal && state4FlatNormal){
+            state5LandscapeNormalInverse = true;
+
+            state2FlatInverse = false;
+            state3LandscapeInverse = false;
+            state4FlatNormal = false;
+        }
+
+        return state5LandscapeNormalInverse;
+    }
+
+
+    /* raise a little the smartphone */
+    boolean raise = false;
+    public boolean raiseALittleSmartphone(){
+        if(wasSmartphoneLandscapeNormal && accelerometerX == 5){
+            raise = true;
+            //isRaise = true;
+        }
+        else{
+            if(raise && smartphoneLandscapeNormal){isRaise = true; raise = false;}
+            else{isRaise = false;}
+        }
+
+        return isRaise;
+    }
+
+    public boolean isPortraitNormal() {
+        return smartphonePortraitNormal2;
+    }
+    public boolean isLandscapeNormal() {
+        return smartphoneLandscapeNormal2;
+    }
+    public boolean isPortraitInverse() {
+        return smartphonePortraitInverse2;
+    }
+    public boolean isLandscapeInvese() {
+        return smartphoneLandscapeInverse2;
+    }
+}
